97a4129e0c42c9a077e2938f3427c4a6e8578a2c
Avinash <avinashkmg1310@gmail.com>
2024-11-11 19:41:18 +0530

    task - meetup details dynamic routing

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,23 +1,26 @@
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
+import Link from "next/link";
 
 function MeetupItem(props) {
-  return (
-    <li className={classes.item}>
-      <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-        </div>
-        <div className={classes.actions}>
-          <button>Show Details</button>
-        </div>
-      </Card>
-    </li>
-  );
+	return (
+		<li className={classes.item}>
+			<Card>
+				<div className={classes.image}>
+					<img src={props.image} alt={props.title} />
+				</div>
+				<div className={classes.content}>
+					<h3>{props.title}</h3>
+					<address>{props.address}</address>
+				</div>
+				<div className={classes.actions}>
+					<Link href={props.id.toString()}>
+						<button>Show Details</button>
+					</Link>
+				</div>
+			</Card>
+		</li>
+	);
 }
 
 export default MeetupItem;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,17 @@
+import { useRouter } from "next/router";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
 const MeetingDetails = () => {
-	return <div>MeetingDetails</div>;
+	const router = useRouter();
+	const { meetupId } = router.query;
+	return (
+		<MeetupDetail
+			image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+			title="First Meetup"
+			address="Some Street 5, Some City"
+			description="This is a first meetup"
+		/>
+	);
 };
 
 export default MeetingDetails;
