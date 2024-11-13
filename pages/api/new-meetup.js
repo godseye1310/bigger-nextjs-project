136d732c8b65d5a84758959c301517a432af3ebe
Avinash <avinashkmg1310@gmail.com>
2024-11-13 18:44:17 +0530

    post api + mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,15 +1,29 @@
 // POST // /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
 	if (req.method === "POST") {
 		const data = req.body;
 		console.log(data);
-		const { title, image, address, description } = data;
+		// const { title, image, address, description } = data;
 
 		// store them to DB
+		const client = await MongoClient.connect(
+			"mongodb+srv://avinash:pVGSNUkIcyY6tKOU@cluster0.xdel4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+		);
 
+		const db = client.db();
+		const meetupsCollection = db.collection("meetups");
+		const result = await meetupsCollection.insertOne(data);
+		console.log(result);
+		client.close();
+
+		// send response
 		res.status(201).json({ message: "Meetup Created!" });
 	}
 }
 
 export default handler;
+
+// mongodb+srv://avinash:<db_password>@cluster0.xdel4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
