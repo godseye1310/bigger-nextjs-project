e8361ca2e7046d4bdff73668a2bff8ae854d2049
Avinash <avinashkmg1310@gmail.com>
2024-11-14 00:02:33 +0530

    (fetch paths and data)Making the Details page work properly

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,62 +1,71 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetingDetails = (props) => {
-	return (
-		<MeetupDetail
-			image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-			title="First Meetup"
-			address="Some Street 5, Some City"
-			description="This is a first meetup"
-		/>
-	);
+const MeetingDetails = ({ meetupData }) => {
+	// console.log(meetupData);
+	// const {image, title, address, description} = meetupData
+
+	return <MeetupDetail {...meetupData} />;
 };
 
 export async function getStaticPaths() {
 	// in real world we would fetch path data from an API
 
+	const response = await fetch("http://localhost:3000/api/fetch-meetups", {
+		method: "GET",
+	});
+	const data = await response.json();
+
+	// Get the paths we want to pre-render based on posts
+	const paths = data.meetups.map((meetup) => ({
+		params: { meetupId: meetup._id.toString() },
+	}));
+
 	return {
-		paths: [
-			{
-				params: {
-					meetupId: "m1",
-				},
-			},
-			{
-				params: {
-					meetupId: "m2",
-				},
-			},
-			{
-				params: {
-					meetupId: "m3",
-				},
-			},
-		],
+		paths,
 
 		// if fallback is true, Next.js will generate paths dynamically at the request time
 		// if fallback is false, Next.js will generate pages for all paths at build time and fallback to the 404 page
-		fallback: "false",
+		// if fallback is "blocking", Next.js will generate paths dynamically at the request time
+		fallback: "blocking",
 	};
 }
 
 export async function getStaticProps(context) {
 	const meetupId = context.params.meetupId;
+	console.log("meetupId: ", meetupId);
+
+	// Convert meetupId to ObjectId
+	const objectId = ObjectId.createFromHexString(meetupId);
 
 	// fetch data from an API
+	const client = await MongoClient.connect(
+		"mongodb+srv://avinash:pVGSNUkIcyY6tKOU@cluster0.xdel4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+	);
+	const db = client.db();
+	const meetupsCollection = db.collection("meetups");
+	const meetupDetails = await meetupsCollection.findOne({
+		_id: objectId,
+	});
+	console.log("meetupDetails: ", meetupDetails);
+
+	const meetupData = {
+		id: meetupDetails._id.toString(),
+		title: meetupDetails.title,
+		image: meetupDetails.image,
+		address: meetupDetails.address,
+		description: meetupDetails.description,
+	};
+
+	client.close();
 
 	return {
 		props: {
-			meetupData: {
-				id: meetupId,
-				title: "First Meetup",
-				image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-				address: "Some Street 5, Some City",
-				description: "This is a first meetup",
-			},
+			meetupData: meetupData,
 		},
 
 		// for Incremental Static Regeneration (ISR) add the revalidate option.
-		revalidate: 9600, // revalidate every 9600 seconds when the new data is added after build
+		revalidate: 1, // revalidate every 9600 seconds when the new data is added after build
 	};
 }
 
